3bc86bb00d4d0e7c1de18fbb7fa6813c74771e23
Xiaohe Dong <ddong@zendesk.com>
2019-06-17 15:27:59 +1000

    minor change

--- leetcode/295-Find-Median-from-Data-Stream.py
+++ leetcode/295-Find-Median-from-Data-Stream.py
@@ -6,9 +6,9 @@ class MedianFinder(object):
 
     def addNum(self, num):
         small, large = self.heaps
-        minNumber = heappushpop(large, num)
-        heappush(small, -minNumber)
-        if len(large) < len(small):
+        minNumber = heappushpop(large, num) # push and pop means refresh
+        heappush(small, -minNumber) # push smaller elements into small
+        if len(large) < len(small): # gurantee large is always greater or equal to smaller
             heappush(large, -heappop(small))
 
     def findMedian(self):
